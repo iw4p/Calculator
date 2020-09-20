bb93a1773a7c36fc353c342b7386cf5f3feef044
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-09-20 23:50:29 +0430

    init commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Nima Akbarzade on 9/20/20.
+//  Copyright © 2020 padidar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
